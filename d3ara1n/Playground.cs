903d716ba0a7ab8ee439f0067d2c4d775da22299
Chien Zhang <zqy0224@live.com>
2023-03-29 21:22:26 +0800

    fmt and make it biggert

--- d3ara1n/Playground.cs
+++ d3ara1n/Playground.cs
@@ -1,11 +1,19 @@
 using System;
-using Internal;
 
-for (int i = 1; i < 10; i++)
+namespace NineNineTable
 {
-    for (int j = 1; j < 10; j++)
+    public class Program
     {
-        Console.Write($"{0}*{1}={2}" + j != 9 ? " " : "", i, j, i * j);
+        public static void Main()
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                for (int j = 1; j < 10; j++)
+                {
+                    Console.Write($"{0}*{1}={2}" + j != 9 ? " " : "", i, j, i * j);
+                }
+                Console.WriteLine();
+            }
+        }
     }
-    Console.WriteLine();
 }
