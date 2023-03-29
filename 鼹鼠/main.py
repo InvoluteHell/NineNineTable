dc31862500520c81926889c8a020446bb00474ff
mole828 <mrx800@163.com>
2023-03-29 20:05:06 +0800

    修改提交

--- 鼹鼠/main.py
+++ 鼹鼠/main.py
@@ -1,1 +1,1 @@
-print('\n'.join([' '.join(['{}*{}={}'.format(j,i,i*j) for j in range(1,i+1)]) for i in range(1,10)]))
+print('\n'.join(['   '.join(['{}x{}={}'.format(j,i,i*j) for j in range(1,i+1)]) for i in range(1,10)]))
