510384d7f1f59344c8f9b04c84a239a583184bc4
Helloworld <1326521+LYZhelloworld@users.noreply.github.com>
2023-03-29 21:38:18 +0800

    NineNineTable program by Helloworld

---
+++ Helloworld/NineNineTable/NineNineTable.Test/NumberMultiplier/NumberPairMultiplierTest.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="NumberPairMultiplierTest.cs" company="Helloworld">
+// Copyright (c) Helloworld. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace NineNineTable.Test.NumberMultiplier
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using NineNineTable.NumberMultiplier;
+
+    /// <summary>
+    /// Tests <see cref="NumberPairMultiplier"/>.
+    /// </summary>
+    [TestClass]
+    public class NumberPairMultiplierTest
+    {
+        /// <summary>
+        /// Tests <see cref="NumberPairMultiplier.Result"/>.
+        /// </summary>
+        [TestMethod]
+        public void TestResult()
+        {
+            new NumberPairMultiplier(3, 7).Result.Should().Be(21);
+        }
+    }
+}
---
+++ Helloworld/NineNineTable/NineNineTable/OutputFormatter/IOutputFormatter.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="IOutputFormatter.cs" company="Helloworld">
+// Copyright (c) Helloworld. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace NineNineTable.OutputFormatter
+{
+    /// <summary>
+    /// The output formatter.
+    /// </summary>
+    /// <typeparam name="T">The type of the item.</typeparam>
+    public interface IOutputFormatter<T>
+    {
+        /// <summary>
+        /// Gets the function to output a string.
+        /// </summary>
+        public Action<string?> OutputFunction { get; init; }
+
+        /// <summary>
+        /// Outputs an item.
+        /// </summary>
+        /// <param name="item">The item.</param>
+        void Output(T item);
+    }
+}
