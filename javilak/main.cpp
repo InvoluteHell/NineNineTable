ffb111552982ad4e79cba69aef3f2205dbd2e26c
javilak <47470153+javilak@users.noreply.github.com>
2023-03-29 23:33:14 +0800

    Update main.cpp

--- javilak/main.cpp
+++ javilak/main.cpp
@@ -1,18 +1,4 @@
 #include <iostream>
 using namespace std;
-void cx(int a,int b){
-    int c;
-    c=a*b;
-    cout << a <<"*"<<b<<"="<<c;
-    if (c>=10){
-cout<<"  ";
-    }else{
-        cout<<"   ";
-    }}
-int main() {
-for(int i=1;i<=9;i++){
-    for (int j = 1; j <= i; ++j) {
-        cx(i,j);
-    }
-    cout<<"\n";
-}}
+void cx(int a,int b){int c;c=a*b;cout << a <<"*"<<b<<"="<<c;if (c>=10){cout<<"  ";}else{cout<<"   ";}}
+int main() {for(int i=1;i<=9;i++){for (int j = 1; j <= i; ++j) {cx(i,j);}cout<<"\n";}}
