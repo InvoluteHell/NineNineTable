a234af8d66c7ed6e6f91ef577c749603667aad4f
kokic <kokicplus@gmail.com>
2023-03-30 23:57:27 +0800

    Update ninenine.lean

--- kokic/ninenine.lean
+++ kokic/ninenine.lean
@@ -1,1 +1,1 @@
-open List def main:=println! range 9|>map (·+1)|>foldl (λ x y=>x++foldl (λ s t=>s!"{s}{t}*{y}={t*y}\t") "" (range y|>map (·+1))++"\n") ""
+open List #eval println! range 9|>map (·+1)|>foldl (λ x y=>x++foldl (λ s t=>s!"{s}{t}*{y}={t*y}\t") "" (range y|>map (·+1))++"\n") ""
