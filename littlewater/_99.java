bda15d1126c4beba60a82bd2a59a204996e6d586
LittleWater <96013161+littlewater001@users.noreply.github.com>
2023-03-31 23:24:51 +0800

    Create _99.java
    水一个

---
+++ littlewater/_99.java
@@ -0,0 +1,10 @@
+public class _99 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.printf("%d*%d=%-3d", j, i, i * j);
+            }
+            System.out.println();
+        }
+    }
+}
